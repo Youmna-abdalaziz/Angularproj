139f4c6e3327b18f5d3aa048c0b45ac13ad1a12e
Youmna <youmnaabdalaziz@gmail.com>
2019-04-01 21:23:23 +0200

    Add header and footer to home page and route to home page

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import {FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  {path: 'footer',component:FooterComponent },
+  {path: 'header' ,component:HeaderComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

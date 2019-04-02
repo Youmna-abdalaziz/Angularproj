77a684e571c537caa6065772e16bea1446f2a325
Youmna <youmnaabdalaziz@gmail.com>
2019-04-02 23:09:12 +0200

    Create regestir form

--- app/components/regestir-form/regestir-form.component.ts
+++ app/components/regestir-form/regestir-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Validator,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-regestir-form',
@@ -6,8 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./regestir-form.component.scss']
 })
 export class RegestirFormComponent implements OnInit {
+  public regestirForm = new FormGroup({
+    userName: new FormControl('',[Validators.required]),
+    email: new FormControl('',[Validators.required]),
+    password: new FormControl('',Validators.required)
+  })
 
   constructor() { }
+  regestir() {
+    alert("You regestired");
+  }
 
   ngOnInit() {
   }
